8a4393fcd0a7fa6442e285e8359ce7f36ca4939b
AlexAvalosxP <AlexAvalosxP@users.noreply.github.com>
2018-08-13 10:41:47 -0500

    images

--- js/funciones.js
+++ js/funciones.js
@@ -9,15 +9,22 @@ function showTxt(opt)
 	switch (opt)
 	{
 		case 1:
-			info.innerHTML = '<h2>Introducción</h2><p>Los 7 hábitos de la gente altamente efectiva son los siguientes:</p><ul>' +
-			'<li>El hábito de la proactividad nos da la libertad para poder escoger nuestra respuesta a los estímulos del medioambiente. Nos faculta para responder de acuerdo con nuestros principios y valores. En esencia, es lo que nos hace humanos y nos permite afirmar que somos los arquitectos de nuestro propio destino.</li>' +
-			'<li>Comenzar con un fin en mente hace posible que nuestra vida tenga razón de ser, pues la creación de una visión de lo que queremos lograr permite que nuestras acciones estén dirigidas a lo que verdaderamente es significativo en nuestras vidas.</li>' +
-			'<li>Poner primero lo primero nos permite liberarnos de la tiranía de lo urgente para dedicar tiempo a las actividades que en verdad dan sentido a nuestras vidas. Es la disciplina de llevar a cabo lo importante, lo cual nos permite convertir en realidad la visión que forjamos en el hábito 2.</li>' +
-			'<li>Pensar en Ganar-Ganar nos permite desarrollar una mentalidad de abundancia material y espiritual, pues nos cuestiona la premisa de que la vida es un “juego de suma cero” donde para que yo gane alguien tiene que perder.</li>' +
-			'<li>Buscar entender primero y ser entendido después es la esencia del respeto a los demás. La necesidad que tenemos de ser entendidos es uno de los sentimientos más intensos de todos los seres humanos. Este hábito es la clave de las relaciones humanas efectivas y posibilita llegar a acuerdos de tipo Ganar-Ganar.</li>' +
-			'<li>Sinergizar es el resultado de cultivar la habilidad y la actitud de valorar la diversidad. La síntesis de ideas divergentes produce ideas mejores y superiores a las ideas individuales. El logro del trabajo en equipo y la innovación son el resultado de este hábito.</li>' +
-			'<li>Afilar la sierra es usar la capacidad que tenemos para renovarnos física, mental y espiritualmente. Es lo que nos permite establecer un equilibrio entre todas las dimensiones de nuestro ser, a fin de ser efectivos en los diferentes papeles (roles) que desempeñamos en nuestras vidas.</li>' +
-			'</ul>';
+			info.innerHTML = '<h2>Introducción</h2><h2>Los 7 hábitos de la gente altamente efectiva son los siguientes:</h2>' +
+			'<h3 class="der">El hábito de la proactividad nos da la libertad para poder escoger nuestra respuesta a los estímulos del medioambiente. Nos faculta para responder de acuerdo con nuestros principios y valores. En esencia, es lo que nos hace humanos y nos permite afirmar que somos los arquitectos de nuestro propio destino.</h3>' +
+			'<div class="imgH1"></div>' +
+			'<h3 class="izq">Comenzar con un fin en mente hace posible que nuestra vida tenga razón de ser, pues la creación de una visión de lo que queremos lograr permite que nuestras acciones estén dirigidas a lo que verdaderamente es significativo en nuestras vidas.</h3>' +
+			'<div class="imgH2"></div>' +
+			'<h3 class="der">Poner primero lo primero nos permite liberarnos de la tiranía de lo urgente para dedicar tiempo a las actividades que en verdad dan sentido a nuestras vidas. Es la disciplina de llevar a cabo lo importante, lo cual nos permite convertir en realidad la visión que forjamos en el hábito 2.</h3>' +
+			'<div class="imgH3"></div>' +
+			'<h3 class="izq">Pensar en Ganar-Ganar nos permite desarrollar una mentalidad de abundancia material y espiritual, pues nos cuestiona la premisa de que la vida es un “juego de suma cero” donde para que yo gane alguien tiene que perder.</h3>' +
+			'<div class="imgH4"></div>' +
+			'<h3 class="der">Buscar entender primero y ser entendido después es la esencia del respeto a los demás. La necesidad que tenemos de ser entendidos es uno de los sentimientos más intensos de todos los seres humanos. Este hábito es la clave de las relaciones humanas efectivas y posibilita llegar a acuerdos de tipo Ganar-Ganar.</h3>' +
+			'<div class="imgH5"></div>' +
+			'<h3 class="izq">Sinergizar es el resultado de cultivar la habilidad y la actitud de valorar la diversidad. La síntesis de ideas divergentes produce ideas mejores y superiores a las ideas individuales. El logro del trabajo en equipo y la innovación son el resultado de este hábito.</h3>' +
+			'<div class="imgH6"></div>' +
+			'<h3 class="der">Afilar la sierra es usar la capacidad que tenemos para renovarnos física, mental y espiritualmente. Es lo que nos permite establecer un equilibrio entre todas las dimensiones de nuestro ser, a fin de ser efectivos en los diferentes papeles (roles) que desempeñamos en nuestras vidas.</h3>' +
+			'<div class="imgH7"></div>' +
+			'';
 			break;
 		case 2:
 			info.innerHTML = '<h2>Aplicaciones y Ventajas</h2><p></p>';
